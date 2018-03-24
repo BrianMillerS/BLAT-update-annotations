23edabffffdca18f55d4b22bea0eb36087910a44
Brian Miller <Brismiller@gmail.com>
2018-03-24 16:07:02 -0700

    testing a commit, made a definition for the diff between two fa files

--- blat_and_parse.py
+++ blat_and_parse.py
@@ -11,6 +11,7 @@ def bash_cmd(command):
     proc_stdout = process.communicate()[0].strip()
     print("BASH_cmd_stdout: " + proc_stdout + '\n')
 
+
 def get_annotations_to_update(fasta1_filename,fasta2_filename):
     """
     Returns a dictionary with the differeces between to multifasta files 
@@ -38,12 +39,10 @@ def get_annotations_to_update(fasta1_filename,fasta2_filename):
     print("...{} annotations to update were found".format(len(annotations_to_update)))
     return annotations_to_update
 
+
 annotations_to_update = get_annotations_to_update("Couvillion_Ranges_CDS_FROM2006.fa", "Couvillion_Ranges_CDS_FROM2014.fa",)
 
-
 print("Starting BLAT & gathering now coordinates...")
-
-
 for incorrect_annotation_key in annotations_to_update.keys():
     blat_result_filename = "{}_blat_result.psl".format(incorrect_annotation_key)
 
@@ -73,18 +72,19 @@ print("...new coordinates gathered")
 
 
 print("Editing GTF files...")
+# load in older gtf
 gtf_lines=[]
 with open("all_Couvillion_Ranges_for_RNAi_FROM2006.gtf",'r') as openhandle:  # open old gtf file
     reader = csv.reader(openhandle,delimiter='\t')
     for i in reader:
-    	gtf_lines.append(i)  # create a data structure that is [['item0','item0'],['item0','item0']]
-
+    	gtf_lines.append(i)  # create a data structure that is [['line1_item0','line1_item1'...],['line2_item0','line2_item2'...]]
+# alter gtf with new coordinates
 for new_anot_key in updated_annotations:  # for every annotation to update
     for line in gtf_lines:
         if new_anot_key == line[8].split('"')[1]:  # if annotation name in gtf = annotation name in hash table
-            line[0]=updated_annotations[new_anot_key][0]  # write the scaffold
-            line[3]=str(updated_annotations[new_anot_key][1])  # write the start position
-            line[4]=str(updated_annotations[new_anot_key][2])  # write the end position
+            line[0]=updated_annotations[new_anot_key][0]  # change the scaffold
+            line[3]=str(updated_annotations[new_anot_key][1])  # change the start position
+            line[4]=str(updated_annotations[new_anot_key][2])  # change the end position
 
 # write changes to a new gtf
 filename = 'all_Couvillion_Ranges_for_RNAi_FROM2014_made.gtf'
