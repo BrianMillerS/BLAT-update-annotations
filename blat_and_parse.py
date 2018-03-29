c91285e9f095b31155073cdb336fa5f8c1fa9793
Brian Miller <Brismiller@gmail.com>
2018-03-29 11:48:38 -0700

    made the CDS fa be created at start, also added notes

--- blat_and_parse.py
+++ blat_and_parse.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+"""
+This script takes in three inputs (an older version of a genome, a newer version of a genome,
+and a gtf file made from the older genome). This script makes a new gtf file with the 
+updated annotations using the newer version of the genome. This script uses Biopython and
+the command line BLAT ('blat - Standalone BLAT v. 36x2 fast sequence search command line tool').
+"""
 
 from Bio import SeqIO,SearchIO
 import Bio.SearchIO.BlatIO as BlatIO
@@ -7,9 +13,12 @@ import os
 import csv
 
 def bash_cmd(command):
+    """
+    Calls a bash command and returns the standard output.
+    """
     process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, cwd=os.curdir)
     proc_stdout = process.communicate()[0].strip()
-    print("BASH_cmd_stdout: " + proc_stdout + '\n')
+    print("BASH_cmd_stdout: {}".format(proc_stdout))
 
 
 def get_annotations_to_update(fasta1_filename,fasta2_filename):
@@ -18,7 +27,7 @@ def get_annotations_to_update(fasta1_filename,fasta2_filename):
     that have all the same headerlines but maybe different sequences
     The dictionnary has sequences from the first file
     """
-    print("...determining which annotations to update")
+    print("\nDetermining which annotations to update")
     fasta1 = SeqIO.parse(fasta1_filename, "fasta")
     fasta2 = SeqIO.parse(fasta2_filename, "fasta")
 
@@ -28,21 +37,34 @@ def get_annotations_to_update(fasta1_filename,fasta2_filename):
         dic_1[str(i.id)] = str(i.seq)
     for i in fasta2:
         dic_2[str(i.id)] = str(i.seq)
-    
+
     # get annotations that need to be updated
     annotations_to_update = {}
     for i in dic_1.keys():
-        if dic_1[i] == dic_2[i]:
-            continue
-        else:
+        if i not in dic_2.keys():
             annotations_to_update[i] = dic_1[i]
+        else:
+            if dic_1[i] != dic_2[i]:
+                annotations_to_update[i] = dic_1[i]
+
     print("...{} annotations to update were found".format(len(annotations_to_update)))
+    print("...the following annotations need to be updated:")
+    for i in annotations_to_update.keys():
+        print(i)
+    print("...the genomic sequences for these annotations were extracted from the 2006 genome\n")
     return annotations_to_update
 
 
-annotations_to_update = get_annotations_to_update("Couvillion_Ranges_CDS_FROM2006.fa", "Couvillion_Ranges_CDS_FROM2014.fa",)
+# Step1: Determine which annotation coordinates need to be updated
+old_gtf_filename = "Couvillion_sRNA_precursor_annotations_for_2006_genome.gtf"
+print("\nMaking CDS files for each genome version using the gtf file")
+print("...making CDS fa using 2006 genome")
+bash_cmd("~/opt/bin/gffread {} -g TTA1_200603_genome_altered_headerlines.fa -w Couvillion_Ranges_CDS_oldGTF_FROM2006.fa".format(old_gtf_filename))
+print("...making CDS fa using 2014 genome")
+bash_cmd("~/opt/bin/gffread {} -g T_thermophila_June2014_assembly.fa -w Couvillion_Ranges_CDS_oldGTF_FROM2014.fa".format(old_gtf_filename))
+annotations_to_update = get_annotations_to_update("Couvillion_Ranges_CDS_oldGTF_FROM2006.fa", "Couvillion_Ranges_CDS_oldGTF_FROM2014.fa")
 
-print("Starting BLAT & gathering now coordinates...")
+print("Starting BLAT & gathering now coordinates")
 for incorrect_annotation_key in annotations_to_update.keys():
     blat_result_filename = "{}_blat_result.psl".format(incorrect_annotation_key)
 
@@ -54,7 +76,7 @@ for incorrect_annotation_key in annotations_to_update.keys():
 
     # run BLAT (writes output to file)
     bash_cmd("~/opt/bin/blat T_thermophila_June2014_assembly.fa {} -t=dna -q=dna {}".format(querry_filename,blat_result_filename))  # BLAT 
-    
+
     #parse BLAT file
     updated_annotations = {}  # new dictionary for annotation name and the new coordinates
     qresult = SearchIO.read(blat_result_filename, 'blat-psl')
@@ -67,17 +89,16 @@ for incorrect_annotation_key in annotations_to_update.keys():
                     start = HSP.hit_start
                     end = HSP.hit_end
     updated_annotations[incorrect_annotation_key] = [scaffold,start,end]
-    break
-print("...new coordinates gathered")
+print("...new coordinates gathered\n")
 
 
-print("Editing GTF files...")
+print("Editing GTF files\n")
 # load in older gtf
 gtf_lines=[]
-with open("all_Couvillion_Ranges_for_RNAi_FROM2006.gtf",'r') as openhandle:  # open old gtf file
+with open(old_gtf_filename,'r') as openhandle:  # open old gtf file
     reader = csv.reader(openhandle,delimiter='\t')
     for i in reader:
-    	gtf_lines.append(i)  # create a data structure that is [['line1_item0','line1_item1'...],['line2_item0','line2_item2'...]]
+        gtf_lines.append(i)  # create a data structure that is [['line1_item0','line1_item1'...],['line2_item0','line2_item2'...]]
 # alter gtf with new coordinates
 for new_anot_key in updated_annotations:  # for every annotation to update
     for line in gtf_lines:
@@ -87,12 +108,28 @@ for new_anot_key in updated_annotations:  # for every annotation to update
             line[4]=str(updated_annotations[new_anot_key][2])  # change the end position
 
 # write changes to a new gtf
-filename = 'all_Couvillion_Ranges_for_RNAi_FROM2014_made.gtf'
-with open(filename, 'w+') as file:
+new_gtf_filename = "all_Couvillion_Ranges_for_RNAi_FROM2014_made.gtf"
+with open(new_gtf_filename, 'w+') as file:
     for line in gtf_lines:
         file.writelines("\t".join(line)+"\n")
-
+print("...new GTF file with the updated coordinates was written to {} \n".format(new_gtf_filename))
 
 # make fasta from gtf and check
+print("To verify that BLAT functioned as anticipated a new fa file from the new annotations and the 2014 genome...")
+print("will be made and compared to the fa from the 2006 genome and the original annotations.")
+bash_cmd("~/opt/bin/gffread {} -g T_thermophila_June2014_assembly.fa -w Couvillion_Ranges_CDS_FROM2014_check_after_update.fa".format(new_gtf_filename))
 
-print("Script Done.")
+dif_dic_after_update = get_annotations_to_update("Couvillion_Ranges_CDS_oldGTF_FROM2006.fa","Couvillion_Ranges_CDS_FROM2014_check_after_update.fa")
+
+print("\nPrinting annotations that resulted in a different sequence")
+for i in dif_dic_after_update.keys():
+	print(i)
+	print(dif_dic_after_update[i])
+	print("\n")
+
+
+print("\nMoving new files to /blat_querry_files folder")
+bash_cmd("mkdir blat_querry_files")
+bash_cmd("mv *_querry.fa *_blat_result.psl blat_querry_files")
+
+print("\nScript Done.")   
